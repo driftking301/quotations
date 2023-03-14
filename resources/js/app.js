12e00bc17067aec511163e5d6aeabaf44339ca1f
driftking301 <driftking301@gmail.com>
2023-03-14 16:35:39 -0600

    Added app.css and app.js
    Moved files to the correct folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,22 @@
 import './bootstrap';
+import $ from 'jquery';
+window.jQuery = window.$ = $;
 
+import "../../node_modules/select2/dist/css/select2.css";
+import select2 from 'select2';
+select2();
+
+import '../css/app.css';
+
+$(document).ready(function() {
+    $('.select2').select2({
+        placeholder: "Select...",
+        allowClear: true
+    });
+});
+
+$(document).ready(function() {
+    $(function () {
+        // $('[data-toggle="tooltip"]').tooltip();
+    });
+});
